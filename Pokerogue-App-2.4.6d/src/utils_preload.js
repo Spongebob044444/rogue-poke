1ba45e6cdbd0ccc02c36363f0ac6768a5f0a7858
Spongebob044444 <kaito.priv@gmail.com>
2025-10-25 13:24:15 +0200

    Delete Pokerogue-App-2.4.6d directory

--- Pokerogue-App-2.4.6d/src/utils_preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-// Expose ipcRenderer to the renderer process
-contextBridge.exposeInMainWorld('ipcRenderer', {
-  send: (channel, data) => ipcRenderer.send(channel, data)
-});
